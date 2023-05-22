90def7322fe191e19562b3cd78e94b285df6d264
Benedikt <benedikt.geiger00@gmail.com>
2023-05-22 17:07:27 +0200

    Add androidx support

--- app/src/main/java/com/hackermb/smartwatchmetronome/MainActivity.java
+++ app/src/main/java/com/hackermb/smartwatchmetronome/MainActivity.java
@@ -5,7 +5,8 @@ import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
